fb91f9008e0095d8495f70bc1a1aa77b818e90db
wyp-sky <319923341@qq.com>
2019-08-27 10:36:49 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,4 +1,9 @@
 from django.http import Httpresponse
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResponse('index')
+
+def login(requets):
+    return redirect('/index')
+
